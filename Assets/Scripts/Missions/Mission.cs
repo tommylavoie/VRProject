3cb2b2c025621f154548dff24234abf547c05d5f
tommylavoie <tom.patate@hotmail.com>
2016-03-24 12:17:32 -0400

    Mission Start

---
+++ Assets/Scripts/Missions/Mission.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mission : MonoBehaviour
+{
+    public GoalComposite goals;
+
+    void Start()
+    {
+        goals.OnStart();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(goals.IsDone())
+        {
+            OnEnd();
+        }
+	}
+
+    public void OnEnd()
+    {
+
+    }
+}
